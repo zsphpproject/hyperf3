25c2e68add0f9c1985a89ac9c138fc3ac810482a
张帅 <5346812+zhangshuaiii@user.noreply.gitee.com>
2023-09-18 17:29:18 +0800

    ClientParam.php修改

--- src/constant/ClientParam.php
+++ src/constant/ClientParam.php
@@ -2,13 +2,14 @@
 
 namespace Zsgogo\constant;
 
+use Hyperf\Context\Context;
 use Hyperf\HttpServer\Contract\RequestInterface;
 use Zsgogo\utils\Pojo;
 use Zsgogo\utils\SnowFlakeUtil;
 
 class ClientParam extends Pojo {
     public function __construct(RequestInterface $request, array $param = []) {
-        $param["request_id"] = SnowFlakeUtil::getInstance()->getCurrentId();
+        $param["request_id"] = Context::get("request_id");
         parent::__construct($request, $param);
     }
 }
