178b49758a2ad8933c7ca36b53504bdc723da689
靳陆陆 <a805964774@foxmail.com>
2024-02-29 16:39:14 +0800

    数组对象自动赋值，主要是用于protobuf生成的文件，根据setXxx method的注释自动生成对应的数组对象

--- src/utils/ObjUtil.php
+++ src/utils/ObjUtil.php
@@ -4,9 +4,18 @@ namespace Zsgogo\utils;
 
 use App\common\constant\ErrorNums;
 use Hyperf\Stringable\Str;
+use phpDocumentor\Reflection\DocBlock\Tag;
+use phpDocumentor\Reflection\DocBlock\Tags\Param;
+use phpDocumentor\Reflection\DocBlockFactory;
+use phpDocumentor\Reflection\Type;
+use phpDocumentor\Reflection\Types\Array_;
+use phpDocumentor\Reflection\Types\Compound;
+use phpDocumentor\Reflection\Types\Object_;
 use ReflectionClass;
 use ReflectionException;
+use ReflectionNamedType;
 use ReflectionProperty;
+use Zsgogo\exception\AppException;
 
 class ObjUtil
 {
@@ -69,6 +78,9 @@ class ObjUtil
         foreach ($properties as $property) {
             $propertySnakeName = $property->getName();
             $propertyValue = (isset($inputData[$propertySnakeName]) && $inputData[$propertySnakeName] != "") ? $inputData[$propertySnakeName] : $property->getDefaultValue();
+//            if (!is_scalar($propertyValue) && !is_null($propertyValue)) {
+//                var_dump($property->getDocComment());
+//            }
             if ($propertyValue == null && Str::contains($property->getDocComment(), ['int32', 'float'])) {
                 if (!Str::contains($property->getDocComment(), ['repeated'])) {
                     $propertyValue = 0;
@@ -84,6 +96,30 @@ class ObjUtil
             $reflectionMethod = $reflection->getMethod($setDataFuncName);
             if (!$reflectionMethod->isPublic()) {
                 continue;
+            }
+            $docBlockFactory = DocBlockFactory::createInstance();
+            $docBlock = $docBlockFactory->create($reflectionMethod);
+            foreach ($docBlock->getTagsByName('param') as $paramTag) {
+                /**
+                 *
+                 * @var Param $paramTag
+                 */
+                if ($paramTag->getType() instanceof Compound) {
+                    /**
+                     * @var ?Array_ $a
+                     */
+                    $a = $paramTag->getType()->get(0);
+                    if ($a->getValueType() instanceof Object_) {
+                        $obj = $a->getValueType()->getFqsen()->__toString();
+                        if (is_array($propertyValue)) {
+                            foreach ($propertyValue as &$item) {
+                                $tmp = new $obj();
+                                $this->setDataV2($tmp, $item);
+                                $item = $tmp;
+                            }
+                        }
+                    }
+                }
             }
             $reflectionMethod->invokeArgs($response, [$propertyValue]);
         }
