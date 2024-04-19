64d48484130c7d108289e62ad39a0ecd49040d66
afubao <afubao@gmail.com>
2024-04-19 16:21:17 +0800

    修改pojo

--- src/utils/Pojo.php
+++ src/utils/Pojo.php
@@ -3,18 +3,19 @@ declare(strict_types=1);
 
 namespace Zsgogo\utils;
 
+use Hyperf\Context\Context;
 use Hyperf\Contract\Arrayable;
-use Hyperf\Di\Annotation\Inject;
 use Hyperf\Stringable\Str;
+use Psr\Http\Message\ServerRequestInterface;
 use ReflectionClass;
-use ReflectionException;
 use ReflectionProperty;
-use Hyperf\HttpServer\Contract\RequestInterface;
 use App\common\constant\ErrorNums;
+use RuntimeException;
 use Zsgogo\exception\AppException;
 
 
-abstract class Pojo implements Arrayable {
+abstract class Pojo implements Arrayable
+{
 
     /**
      * @var array $data 数据
@@ -22,107 +23,115 @@ abstract class Pojo implements Arrayable {
     private $data = [];
 
     /**
-     * @var array $validates 验证器
-     */
-    protected $validates = [];
-
-    /**
-     * @var bool $autoValidate 自动验证
-     */
-    protected $autoValidate = true;
-
-    /**
-     * @var array $notFilterField 无需全局过滤的字段
-     */
-    protected $notFilterField = [];
-
-    /**
-     * @var \ReflectionClass
+     * @var ReflectionClass
      */
     private $reflectionClass;
 
+    /**
+     * @var array the keys to identify the data of request in coroutine context
+     */
+    protected array $contextkeys
+        = [
+            'parsedData' => 'http.request.parsedData',
+        ];
 
-    public function toArray(): array {
+    public function __construct(array $param = [])
+    {
+        $inputData = $this->getInputData();
+        if (!empty($param)) {
+            $inputData = array_merge($inputData, $param);
+        }
+        $this->reflectionClass = new ReflectionClass($this);
+        $this->setData($inputData);
+    }
+
+    /**
+     * @return array
+     */
+    public function toArray(): array
+    {
         $properties = $this->reflectionClass->getProperties(ReflectionProperty::IS_PRIVATE);
         foreach ($properties as $property) {
-            $propertyName = $property->getName();
-            $getDataFuncName = 'get' . ucfirst($propertyName);
-            $this->data[Str::snake($propertyName)] = $this->$getDataFuncName();
+            $propertySnakeName = Str::snake($property->getName());
+            if (!isset($this->data[$propertySnakeName])) {
+                $getter = \Hyperf\Support\getter($propertySnakeName);
+                $this->data[$propertySnakeName] = $this->$getter();
+            }
         }
         return $this->data;
     }
 
-
-    public function __construct(RequestInterface $request, array $param = []) {
-        $inputData = $request->all();
-        $inputData = isset($inputData[0]) ? $inputData[0] : $inputData;
-        $inputData = $this->fitterData($inputData ?? []);
-        if ($request->getMethod() == "GET") {
-            $getArray = $request->all();
-            foreach ($getArray as $key => $value){
-                if ($value == "") unset($getArray[$key]);
-            }
-            $inputData = array_merge($inputData,$getArray);
-        }
-
-        if (!empty($param)) {
-            $inputData = array_merge($inputData,$param);
-        }
-
-
-        $this->reflectionClass = new ReflectionClass($this);
-        $this->setData($inputData);
-        // if ($this->autoValidate) {
-        //     $this->validate();
-        // }
+    /**
+     * @return array
+     */
+    public function all(): array
+    {
+        return $this->getInputData();
     }
-
-
-    // public function validate() {
-    //     foreach ($this->validates as $validate => $scene) {
-    //         if (is_string($scene)) {
-    //             validate($validate)->scene($scene)->check($this->toArray());
-    //         } else if (is_array($scene)) {
-    //             foreach ($scene as $item) {
-    //                 validate($validate)->scene($item)->check($this->toArray());
-    //             }
-    //         }
-    //     }
-    // }
-
-
-    public function fitterData(array $params): array {
-        foreach ($params as $paramKey => $paramValue) {
-            if (in_array($paramKey, $this->notFilterField)) {
-                unset($params[$paramKey]);
-            }
-        }
-        return $params;
-    }
-
 
     /**
      * @throws AppException
-     * @throws ReflectionException
      */
-    private function setData($inputData) {
+    private function setData($inputData): void
+    {
         $properties = $this->reflectionClass->getProperties(ReflectionProperty::IS_PRIVATE);
         foreach ($properties as $property) {
             $propertySnakeName = Str::snake($property->getName());
             if (isset($inputData[$propertySnakeName])) {
                 $propertyValue = $inputData[$propertySnakeName] != "" ? $inputData[$propertySnakeName] : $property->getDefaultValue();
-                $propertyName = $property->getName();
-                $setDataFuncName = 'set' . ucfirst($propertyName);
-                if (!$this->reflectionClass->hasMethod($setDataFuncName)) {
-                    throw new AppException(ErrorNums::METHOD_NOT_EXISTS,'method ' . $this->reflectionClass->getName() . '::' . $setDataFuncName . ' not exists!');
-                }
-                $reflectionMethod = $this->reflectionClass->getMethod($setDataFuncName);
-                if (!$reflectionMethod->isPublic()) {
-                    throw new AppException(ErrorNums::METHOD_NOT_PUBLIC,'method ' . $this->reflectionClass->getName() . '::' . $setDataFuncName . ' is not public!');
-                }
-                $reflectionMethod->invokeArgs($this, [$propertyValue]);
+                $property->setValue($this, $propertyValue);
+                $this->data[$propertySnakeName] = $propertyValue;
             }
-
         }
+    }
+
+    /**
+     * @return ServerRequestInterface
+     */
+    protected function getRequest(): ServerRequestInterface
+    {
+        return Context::get(ServerRequestInterface::class);
+    }
+
+    /**
+     * @param $name
+     * @param $arguments
+     * @return mixed
+     */
+    protected function call($name, $arguments)
+    {
+        $request = $this->getRequest();
+        if (! method_exists($request, $name)) {
+            throw new RuntimeException('Method not exist.');
+        }
+        return $request->{$name}(...$arguments);
+    }
+
+    /**
+     * @return array
+     */
+    protected function getInputData(): array
+    {
+        return $this->storeParsedData(function () {
+            $request = $this->getRequest();
+            if (is_array($request->getParsedBody())) {
+                $data = $request->getParsedBody();
+            } else {
+                $data = [];
+            }
+            return array_merge($data, $request->getQueryParams());
+        });
+    }
+
+    /**
+     * @param callable $callback
+     * @return mixed
+     */
+    protected function storeParsedData(callable $callback): mixed
+    {
+        if (! Context::has($this->contextkeys['parsedData'])) {
+            return Context::set($this->contextkeys['parsedData'], $callback());
+        }
+        return Context::get($this->contextkeys['parsedData']);
     }
 }
